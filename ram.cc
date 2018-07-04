0ed60432b89483926b8e337d0fbfe1264a4f642e
nagisc007 <nagisc007@yahoo.co.jp>
2018-07-04 14:45:32 +0900

    WIP: Changed: ram method to append validator

--- ram.cc
+++ ram.cc
@@ -13,6 +13,15 @@
 
 namespace CPU {
 
+namespace {
+
+bool _IsValidIndex(T_index i, int size)
+{
+  return i >= 0 && i < size;
+}
+
+}  // inner global
+
 // struct: MemoFile
 MemoFile::MemoFile(const T_str& s):
   modified(true),
@@ -92,26 +101,80 @@ void Ram::Reset()
 }
 
 // controls
-T_id bookIdOf(const Ram* ram, T_id fid, T_index idx)
+bool IsExistsPath(const T_str& path)
 {
-  return fid >= 0 && idx < ram->files.at(fid)->book_ids.size() ?
+  return QFileInfo(path).exists() &&
+      QFileInfo(path).suffix() == COMMON::FILE_EXTENSION;
+}
+
+bool IsValidBookId(const Ram* ram, T_id fid, T_id bid, bool is_validated)
+{
+  return (is_validated || IsValidFileId(ram, fid)) &&
+      _IsValidIndex(bid, ram->books.size()) && ram->files.at(fid)->book_ids.contains(bid);
+}
+
+bool IsValidBookIndex(const Ram* ram, T_id fid, T_index idx, bool is_validated)
+{
+  return (is_validated || IsValidFileId(ram, fid)) &&
+      _IsValidIndex(idx, ram->files.at(fid)->book_ids.size());
+}
+
+bool IsValidFileId(const Ram* ram, T_id fid)
+{
+  return _IsValidIndex(fid, ram->files.size()) && ram->file_ids.contains(fid);
+}
+
+bool IsValidFileIndex(const Ram* ram, T_index idx)
+{
+  return _IsValidIndex(idx, ram->files.size());
+}
+
+bool IsValidName(const T_str& s)
+{
+  return !s.isEmpty() && s != "";
+}
+
+bool IsValidPageId(const Ram* ram, T_id bid, T_id pid, bool is_validated, T_id fid)
+{
+  return (is_validated || IsValidBookId(ram, fid, bid, false)) &&
+      _IsValidIndex(pid, ram->books.at(bid)->page_ids.size()) &&
+      ram->books.at(bid)->page_ids.contains(pid);
+}
+
+bool IsValidPageIndex(const Ram* ram, T_id bid, T_index idx, bool is_validated, T_id fid)
+{
+  return (is_validated || IsValidBookId(ram, fid, bid, false)) &&
+      _IsValidIndex(idx, ram->books.at(bid)->page_ids.size());
+}
+
+bool IsValidPath(const T_str& path)
+{
+  return IsValidName(path) && QFileInfo(path).suffix() == COMMON::FILE_EXTENSION;
+}
+
+T_id bookIdOf(const Ram* ram, T_id fid, T_index idx, bool is_validated)
+{
+  return is_validated ||
+      (IsValidFileId(ram, fid) && IsValidBookIndex(ram, fid, idx, false)) ?
         ram->files.at(fid)->book_ids.at(idx): -1;
 }
 
-T_ids bookIdsOf(const Ram* ram, T_id fid)
+T_ids bookIdsOf(const Ram* ram, T_id fid, bool is_validated)
 {
-  return fid >= 0 ? ram->files.at(fid)->book_ids: T_ids();
+  return is_validated || IsValidFileId(ram, fid) ?
+        ram->files.at(fid)->book_ids: T_ids();
 }
 
-T_index bookIndexOf(const Ram* ram, T_id fid, T_id bid)
+T_index bookIndexOf(const Ram* ram, T_id fid, T_id bid, bool is_validated)
 {
-  return fid >= 0 ? ram->files.at(fid)->book_ids.indexOf(bid): -1;
+  return is_validated || IsValidFileId(ram, fid) ?
+        ram->files.at(fid)->book_ids.indexOf(bid): -1;
 }
 
-T_strs bookLabelsOf(const Ram* ram, T_id fid)
+T_strs bookLabelsOf(const Ram* ram, T_id fid, bool is_validated)
 {
   T_strs res;
-  if (fid < 0) return res;
+  if (!is_validated && !IsValidFileId(ram, fid)) return res;
 
   for (auto& id: ram->files.at(fid)->book_ids) {
     res << ram->books.at(id)->label;
@@ -119,9 +182,9 @@ T_strs bookLabelsOf(const Ram* ram, T_id fid)
   return res;
 }
 
-T_states bookStatesOf(const Ram* ram, T_id fid)
+T_states bookStatesOf(const Ram* ram, T_id fid, bool is_validated)
 {
-  if (fid < 0) return T_states();
+  if (!is_validated && !IsValidFileId(ram, fid)) return T_states();
 
   T_states res(ram->files.at(fid)->book_ids.size());
   int idx = 0;
@@ -137,21 +200,21 @@ T_id currentFileId(const Ram* ram)
   return ram->ireg.at(static_cast<int>(IRegAddr::CURRENT_FILEID));
 }
 
-T_id currentBookId(const Ram* ram)
+T_id currentBookId(const Ram* ram, T_id fid, bool is_validated)
 {
-  auto cur_fid = currentFileId(ram);
-  return cur_fid >= 0 ? ram->files.at(cur_fid)->current_bookid: -1;
+  return is_validated || IsValidFileId(ram, fid) ?
+        ram->files.at(fid)->current_bookid: -1;
 }
 
-T_id currentPageId(const Ram* ram)
+T_id currentPageId(const Ram* ram, T_id bid, bool is_validated, T_id fid)
 {
-  auto cur_bid = currentBookId(ram);
-  return cur_bid >= 0 ? ram->books.at(cur_bid)->current_pageid: -1;
+  return is_validated || IsValidBookId(ram, fid < 0 ? currentFileId(ram): fid, bid, false) ?
+        ram->books.at(bid)->current_pageid: -1;
 }
 
-T_id fileIdOf(const Ram* ram, T_index idx)
+T_id fileIdOf(const Ram* ram, T_index idx, bool is_validated)
 {
-  return idx >= 0 && idx < ram->file_ids.size() ? ram->file_ids.at(idx): -1;
+  return is_validated || IsValidFileIndex(ram, idx) ? ram->file_ids.at(idx): -1;
 }
 
 T_index fileIndexOf(const Ram* ram, T_id fid)
@@ -167,9 +230,9 @@ T_strs fileLabelsOf(const Ram* ram)
   return res;
 }
 
-T_str filePathOf(const Ram* ram, T_id fid)
+T_str filePathOf(const Ram* ram, T_id fid, bool is_validated)
 {
-  return fid >= 0 ? ram->files.at(fid)->path: T_str();
+  return is_validated || IsValidFileId(ram, fid) ? ram->files.at(fid)->path: T_str();
 }
 
 T_states fileStatesOf(const Ram* ram)
@@ -183,26 +246,29 @@ T_states fileStatesOf(const Ram* ram)
   return res;
 }
 
-T_id pageIdOf(const Ram* ram, T_id bid, T_index idx)
+T_id pageIdOf(const Ram* ram, T_id bid, T_index idx, bool is_validated, T_id fid)
 {
-  return bid >= 0 && idx >= 0 && idx < ram->books.at(bid)->page_ids.size() ?
+  return is_validated ||
+      (IsValidBookId(ram, fid, bid, false) && IsValidPageIndex(ram, bid, idx, false, fid)) ?
         ram->books.at(bid)->page_ids.at(idx): -1;
 }
 
-T_ids pageIdsOf(const Ram* ram, T_id bid)
+T_ids pageIdsOf(const Ram* ram, T_id bid, bool is_validated, T_id fid)
 {
-  return bid >= 0 ? ram->books.at(bid)->page_ids: T_ids();
+  return is_validated || IsValidBookId(ram, fid, bid, false) ?
+        ram->books.at(bid)->page_ids: T_ids();
 }
 
-T_index pageIndexOf(const Ram* ram, T_id bid, T_id pid)
+T_index pageIndexOf(const Ram* ram, T_id bid, T_id pid, bool is_validated, T_id fid)
 {
-  return bid >= 0 ? ram->books.at(bid)->page_ids.indexOf(pid): -1;
+  return is_validated || IsValidBookId(ram, fid, bid, false) ?
+        ram->books.at(bid)->page_ids.indexOf(pid): -1;
 }
 
-T_strs pageLabelsOf(const Ram* ram, T_id bid)
+T_strs pageLabelsOf(const Ram* ram, T_id bid, bool is_validated, T_id fid)
 {
   T_strs res;
-  if (bid < 0) return res;
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return res;
 
   for (auto& id: ram->books.at(bid)->page_ids) {
     res << ram->pages.at(id)->label;
@@ -210,9 +276,9 @@ T_strs pageLabelsOf(const Ram* ram, T_id bid)
   return res;
 }
 
-T_states pageStatesOf(const Ram* ram, T_id bid)
+T_states pageStatesOf(const Ram* ram, T_id bid, bool is_validated, T_id fid)
 {
-  if (bid < 0) return T_states();
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return T_states();
 
   T_states res(ram->books.at(bid)->page_ids.size());
   int idx = 0;
@@ -223,78 +289,34 @@ T_states pageStatesOf(const Ram* ram, T_id bid)
   return res;
 }
 
-T_strs pageTextsOf(const Ram* ram, T_id bid)
+T_strs pageTextsOf(const Ram* ram, T_id bid, bool is_validated, T_id fid)
 {
   T_strs res;
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return res;
+
   for (auto& id: ram->books.at(bid)->page_ids) {
     res << ram->pages.at(id)->text;
   }
   return res;
 }
 
-T_str pageTextOf(const Ram* ram, T_id pid)
+T_str pageTextOf(const Ram* ram, T_id pid, bool is_validated, T_id fid, T_id bid)
 {
-  return ram->pages.at(pid)->text;
+  return is_validated || IsValidPageId(ram, bid, pid, false, fid) ?
+        ram->pages.at(pid)->text: T_str();
 }
 
-bool IsExistsPath(const T_str& path)
+bool UpdateBookModified(Ram* ram, T_id bid, bool modified, bool is_validated, T_id fid)
 {
-  return QFileInfo(path).exists() &&
-      QFileInfo(path).suffix() == COMMON::FILE_EXTENSION;
-}
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return false;
 
-bool IsValidBookId(const Ram* ram, T_id fid, T_id bid)
-{
-  return bid >= 0 && bid < ram->books.size() &&
-      ram->files.at(fid)->book_ids.contains(bid);
-}
-
-bool IsValidBookIndex(const Ram* ram, T_id fid, T_index idx)
-{
-  return idx >= 0 && idx < ram->files.at(fid)->book_ids.size();
-}
-
-bool IsValidFileId(const Ram* ram, T_id fid)
-{
-  return fid >= 0 && fid < ram->files.size()
-      && ram->file_ids.contains(fid);
-}
-
-bool IsValidFileIndex(const Ram* ram, T_index idx)
-{
-  return idx >= 0 && idx < ram->files.size();
-}
-
-bool IsValidName(const T_str& s)
-{
-  return !s.isEmpty() && s != "";
-}
-
-bool IsValidPageId(const Ram* ram, T_id bid, T_id pid)
-{
-  return pid >= 0 && pid < ram->books.at(bid)->page_ids.size() &&
-      ram->books.at(bid)->page_ids.contains(pid);
-}
-
-bool IsValidPageIndex(const Ram* ram, T_id bid, T_index idx)
-{
-  return idx >= 0 && idx < ram->books.at(bid)->page_ids.size();
-}
-
-bool IsValidPath(const T_str& path)
-{
-  return IsValidName(path) && QFileInfo(path).suffix() == COMMON::FILE_EXTENSION;
-}
-
-bool UpdateBookModified(Ram* ram, T_id bid, bool modified)
-{
   ram->books.at(bid)->modified = modified;
   return true;
 }
 
-bool UpdateCurrentBookId(Ram* ram, T_id fid, T_id bid)
+bool UpdateCurrentBookId(Ram* ram, T_id fid, T_id bid, bool is_validated)
 {
-  if (fid < 0) return false;
+  if (!is_validated && !IsValidFileId(ram, fid)) return false;
 
   ram->files.at(fid)->current_bookid = bid;
   return true;
@@ -306,30 +328,34 @@ bool UpdateCurrentFileId(Ram* ram, T_id fid)
   return true;
 }
 
-bool UpdateCurrentPageId(Ram* ram, T_id bid, T_id pid)
+bool UpdateCurrentPageId(Ram* ram, T_id bid, T_id pid, bool is_validated, T_id fid)
 {
-  if (bid < 0) return false;
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return false;
 
   ram->books.at(bid)->current_pageid = pid;
   return true;
 }
 
-bool UpdateFileModified(Ram* ram, T_id fid, bool modified)
+bool UpdateFileModified(Ram* ram, T_id fid, bool modified, bool is_validated)
 {
+  if (!is_validated && !IsValidFileId(ram, fid)) return false;
+
   ram->files.at(fid)->modified = modified;
   return true;
 }
 
-bool UpdateFilePath(Ram* ram, T_id fid, const T_str& path)
+bool UpdateFilePath(Ram* ram, T_id fid, const T_str& path, bool is_validated)
 {
-  if (fid < 0) return false;
+  if (!is_validated && !IsValidFileId(ram, fid)) return false;
 
   ram->files.at(fid)->path = path;
   return true;
 }
 
-bool UpdatePageModified(Ram* ram, T_id pid, bool modified)
+bool UpdatePageModified(Ram* ram, T_id pid, bool modified, bool is_validated, T_id fid, T_id bid)
 {
+  if (!is_validated && !IsValidPageId(ram, bid, pid, false, fid)) return false;
+
   ram->pages.at(pid)->modified = modified;
   return true;
 }
@@ -348,6 +374,8 @@ bool AddBook(Ram* ram, T_id bid, const T_str& name)
 
 bool AddFile(Ram* ram, T_id fid, const T_str& name)
 {
+  if (fid < 0) return false;
+
   if (fid < ram->files.size()) {
     ram->files.at(fid)->Refresh(name);
   } else {
@@ -358,6 +386,8 @@ bool AddFile(Ram* ram, T_id fid, const T_str& name)
 
 bool AddPage(Ram* ram, T_id pid, const T_str& name, const T_str& text)
 {
+  if (pid < 0) return false;
+
   if (pid < ram->pages.size()) {
     ram->pages.at(pid)->Refresh(name, text);
   } else {
@@ -366,9 +396,11 @@ bool AddPage(Ram* ram, T_id pid, const T_str& name, const T_str& text)
   return true;
 }
 
-bool AppendBookIds(Ram* ram, T_id fid, T_id bid)
+bool AppendBookIds(Ram* ram, T_id fid, T_id bid, bool is_validated)
 {
-  if (ram->files.at(fid)->book_ids.contains(bid)) return false;
+  if ((!is_validated && !IsValidFileId(ram, fid)) || bid < 0 ||
+      ram->files.at(fid)->book_ids.contains(bid))
+    return false;
 
   ram->files.at(fid)->book_ids.append(bid);
   return true;
@@ -376,81 +408,101 @@ bool AppendBookIds(Ram* ram, T_id fid, T_id bid)
 
 bool AppendFileIds(Ram* ram, T_id fid)
 {
-  if (ram->file_ids.contains(fid)) return false;
+  if (fid < 0 || ram->file_ids.contains(fid)) return false;
 
   ram->file_ids.append(fid);
   return true;
 }
 
-bool AppendPageIds(Ram* ram, T_id bid, T_id pid)
+bool AppendPageIds(Ram* ram, T_id bid, T_id pid, bool is_validated, T_id fid)
 {
-  if (ram->books.at(bid)->page_ids.contains(pid)) return false;
+  if ((!is_validated && !IsValidBookId(ram, fid, bid, false)) || pid < 0 ||
+      ram->books.at(bid)->page_ids.contains(pid))
+    return false;
 
   ram->books.at(bid)->page_ids.append(pid);
   return true;
 }
 
-bool MoveBook(Ram* ram, T_id fid, T_index from, T_index to)
+bool MoveBook(Ram* ram, T_id fid, T_index from, T_index to, bool is_validated)
 {
+  if (!is_validated && !IsValidFileId(ram, fid)) return false;
+
+  // NOTE: should need both indices validated?
   ram->files.at(fid)->book_ids.move(from, to);
   return true;
 }
 
 bool MoveFile(Ram* ram, T_index from, T_index to)
 {
+  // NOTE: should need both indices validated?
   ram->file_ids.move(from, to);
   return true;
 }
 
-bool MovePage(Ram* ram, T_id bid, T_index from, T_index to)
+bool MovePage(Ram* ram, T_id bid, T_index from, T_index to, bool is_validated, T_id fid)
 {
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return false;
+
+  // NOTE: should need both indices validated?
   ram->books.at(bid)->page_ids.move(from, to);
   return true;
 }
 
-bool RemoveBookIds(Ram* ram, T_id fid, T_id bid)
+bool RemoveBookIds(Ram* ram, T_id fid, T_id bid, bool is_validated)
 {
-  if (!ram->files.at(fid)->book_ids.contains(bid)) return false;
+  if ((!is_validated && !IsValidFileId(ram, fid)) ||
+      !ram->files.at(fid)->book_ids.contains(bid))
+    return false;
 
   return ram->files.at(fid)->book_ids.removeOne(bid);
 }
 
 bool RemoveFileIds(Ram* ram, T_id fid)
 {
-  if (!ram->file_ids.contains(fid)) return false;
-
+  // NOTE: removeOne contains a feature to check the fid
   return ram->file_ids.removeOne(fid);
 }
 
-bool RemovePageIds(Ram* ram, T_id bid, T_id pid)
+bool RemovePageIds(Ram* ram, T_id bid, T_id pid, bool is_validated, T_id fid)
 {
-  if (!ram->books.at(bid)->page_ids.contains(pid)) return false;
+  if ((!is_validated && !IsValidBookId(ram, fid, bid, false)) ||
+      !ram->books.at(bid)->page_ids.contains(pid))
+    return false;
 
   return ram->books.at(bid)->page_ids.removeOne(pid);
 }
 
-bool RenameBook(Ram* ram, T_id bid, const T_str& name)
+bool RenameBook(Ram* ram, T_id bid, const T_str& name, bool is_validated, T_id fid)
 {
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return false;
+
   ram->books.at(bid)->label = name;
   return true;
 }
 
-bool RenameFile(Ram* ram, T_id fid, const T_str& name)
+bool RenameFile(Ram* ram, T_id fid, const T_str& name, bool is_validated)
 {
+  if (!is_validated && !IsValidFileId(ram, fid)) return false;
+
   ram->files.at(fid)->path = name;
   return true;
 }
 
-bool RenamePage(Ram* ram, T_id pid, const T_str& name)
+bool RenamePage(Ram* ram, T_id pid, const T_str& name, bool is_validated, T_id fid, T_id bid)
 {
+  if (!is_validated && !IsValidPageId(ram, bid, pid, false, fid)) return false;
+
   ram->pages.at(pid)->label = name;
   return true;
 }
 
-bool SortBooks(Ram* ram, T_id fid, T_order order)
+bool SortBooks(Ram* ram, T_id fid, T_order order, bool is_validated)
 {
+  if (!is_validated && !IsValidFileId(ram, fid)) return false;
+
   auto origin = ram->files.at(fid)->book_ids;
-  T_strs labels = bookLabelsOf(ram, fid);
+  T_strs labels = bookLabelsOf(ram, fid, true);
   T_strs combined;
   for (int i = 0; i < labels.size(); ++i)
     combined << labels.at(i) + COMMON::SPLITTER + QString::number(origin.at(i));
@@ -463,14 +515,17 @@ bool SortBooks(Ram* ram, T_id fid, T_order order)
   for (int i = 0; i < combined.size(); ++i) {
     ids << combined.at(i).section(COMMON::SPLITTER, 1, 1).toInt();
   }
+  // NOTE: mutex needed?
   ram->files.at(fid)->book_ids = ids;
   return true;
 }
 
-bool SortPages(Ram* ram, T_id bid, T_order order)
+bool SortPages(Ram* ram, T_id bid, T_order order, bool is_validated, T_id fid)
 {
+  if (!is_validated && !IsValidBookId(ram, fid, bid, false)) return false;
+
   auto origin = ram->books.at(bid)->page_ids;
-  T_strs labels = pageLabelsOf(ram, bid);
+  T_strs labels = pageLabelsOf(ram, bid, true);
   T_strs combined;
   for (int i = 0; i < labels.size(); ++i) {
     combined << labels.at(i) + COMMON::SPLITTER + QString::number(origin.at(i));
@@ -484,6 +539,7 @@ bool SortPages(Ram* ram, T_id bid, T_order order)
   for (int i = 0; i < combined.size(); ++i) {
     ids << combined.at(i).section(COMMON::SPLITTER, 1, 1).toInt();
   }
+  // NOTE: mutex needed?
   ram->books.at(bid)->page_ids = ids;
   return true;
 }
@@ -507,23 +563,27 @@ T_id PopId(Ram* ram, T_id_addr addr)
           ram->pageid_stack.pop();
     }
   }
-  return -1;
 }
 
 bool PushId(Ram* ram, T_id_addr addr, T_id id)
 {
   switch (addr) {
-  case IdAddr::BOOK:
+  case IdAddr::BOOK: {
+    if (ram->bookid_stack.contains(id)) return false;
     ram->bookid_stack.push(id);
     return true;
-  case IdAddr::FILE:
+  }
+  case IdAddr::FILE: {
+    if (ram->fileid_stack.contains(id)) return false;
     ram->fileid_stack.push(id);
     return true;
-  case IdAddr::PAGE:
+  }
+  case IdAddr::PAGE: {
+    if (ram->pageid_stack.contains(id)) return false;
     ram->pageid_stack.push(id);
     return true;
   }
-  return false;
+  }
 }
 
 }  // ns CPU
